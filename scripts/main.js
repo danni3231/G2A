ca64f7a56fb5c8469ef2bbdd59980d7a4b67a3b3
Daniel Rojas <53954296+danni3231@users.noreply.github.com>
2020-09-27 10:16:59 -0500

    Input Focus
    se agrego el js para manejar el cambio entre clases de la promo

--- scripts/main.js
+++ scripts/main.js
@@ -16,5 +16,19 @@ function handleNextSlide () {
       }
 }
 
+const listInput = document.querySelectorAll('.logos--button input');
+
+function logosInputHandle (event) {
+  listInput.forEach(element => {
+    element.classList.remove('logos--button-focus');
+  });
+  event.target.classList.add('logos--button-focus');
+  console.log(event);
+}
+
+listInput.forEach(function (elem, index) {
+  elem.addEventListener('click', logosInputHandle);
+});
+
 setInterval(handleNextSlide, 1000);
 
