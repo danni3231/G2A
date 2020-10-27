a91a7d5b3544011b1f207336df1c7ebdb5e70e24
Daniel Rojas <53954296+danni3231@users.noreply.github.com>
2020-10-26 22:25:43 -0500

    Sign up and Sign in
    Added the html files for the Register and Login pages and added the css style.

---
+++ scripts/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBZTfiYmODgxwBvuAz7Tc2vDqKR98Bcllg",
+    authDomain: "g2a-54426.firebaseapp.com",
+    databaseURL: "https://g2a-54426.firebaseio.com",
+    projectId: "g2a-54426",
+    storageBucket: "g2a-54426.appspot.com",
+    messagingSenderId: "235301313566",
+    appId: "1:235301313566:web:76e109bcd728b79124e20f",
+    measurementId: "G-384WRHBQR9"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
